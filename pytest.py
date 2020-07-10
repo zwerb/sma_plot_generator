65d2fc5894b7472946b33c901a72030a4eaf13ff
Zwerb LLC <zwerbllc@Zwerb.fios-router.home>
2020-07-10 12:33:23 -0400

    updatinga two arg pytest

--- pytest.py
+++ pytest.py
@@ -1,6 +1,13 @@
-import sys
+#!/usr/bin/env python3
+
+import sys, os
+
+import sys, os
+
 
 if len(sys.argv) == 3:
     print ('You typed in 2:{} 1:{}'.format(sys.argv[2], sys.argv[1]))
 else:
     print ('invalid number of args. argv=[{}]'.format(str(sys.argv)))
+
+print('Bin: '+str(os.path.dirname(sys.executable))
