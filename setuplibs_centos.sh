b269ae23e6233d61c9e72bf625ca0a02f80b04b2
Zwerb LLC <zwerbllc@Zwerb.fios-router.home>
2020-07-10 12:22:28 -0400

    disambiguation

--- setuplibs_centos.sh
+++ setuplibs_centos.sh
@@ -2,19 +2,17 @@
 
 "Installing Environment for Python3 Jupyterlab Stock Analysis"
 
-sudo pip3 install --upgrade pip
+pip3 install --upgrade pip
 
-sudo pip3 install -U jupyter
-sudo pip3 install -U jupyterlab
+pip3 install -U jupyter
+pip3 install -U jupyterlab
 
-sudo pip3 install yfinance --upgrade
-sudo pip3 install numpy --upgrade
-sudo pip3 install matplotlib --upgrade
-sudo pip3 install seaborn --upgrade
-sudo pip3 install pandas --upgrade
-sudo pip3 install yahoofinancials --upgrade
-sudo pip3 install sklearn --upgrade
-sudo pip3 install --no-cache-dir tensorflow --upgrade
-sudo pip3 install keras --upgrade
-
-~                                   
+pip3 install yfinance --upgrade
+pip3 install numpy --upgrade
+pip3 install matplotlib --upgrade
+pip3 install seaborn --upgrade
+pip3 install pandas --upgrade
+pip3 install yahoofinancials --upgrade
+pip3 install sklearn --upgrade
+pip3 install --no-cache-dir tensorflow --upgrade
+pip3 install keras --upgrade                          
